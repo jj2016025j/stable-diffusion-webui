dcf54598d57c74059ef2b50557cb15bd3ade11b7
jj2016025j <103946700+jj2016025j@users.noreply.github.com>
2024-07-31 20:54:43 +0800

    修改安裝方式

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,9 @@
+git pull 
 @echo off
 
-set PYTHON=
+set PYTHON="C:\Users\User\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--medvram --opt-split-attention --precision full --no-half
+set COMMANDLINE_ARGS= --api --medvram --opt-split-attention --precision full --no-half
 
 call webui.bat
